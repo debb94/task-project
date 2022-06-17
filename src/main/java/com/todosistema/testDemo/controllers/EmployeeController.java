581b1a0bb7e3c00c48feb10ad6d2ab9eaa4ef7d7
debb94 <daniel.bolivar.freelance@gmail.com>
2022-06-17 02:41:09 -0500

    change credentials

--- src/main/java/com/todosistema/testDemo/controllers/EmployeeController.java
+++ src/main/java/com/todosistema/testDemo/controllers/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,11 +28,12 @@ public class EmployeeController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getById(@PathVariable("id") Integer id){
-        var res = employeeService.findById(id).get();
-        System.out.println("------------------->"+res.toString());
-        return new ResponseEntity<Employee>(res, HttpStatus.OK);
+        try{
+            return new ResponseEntity<Employee>(employeeService.findById(id).get(), HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-
 
     @GetMapping("/create")
     public Employee create(){
@@ -38,6 +41,11 @@ public class EmployeeController {
         employee.setActive(1);
         employee.setName("Eduardo Barrera");
         return employeeService.save(employee);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Employee> save(@RequestBody Employee employee){
+        return new ResponseEntity<Employee>(employeeService.save(employee),HttpStatus.CREATED);
     }
 
 }
